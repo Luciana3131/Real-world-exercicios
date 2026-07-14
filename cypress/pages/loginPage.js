1f29464b8601cff1f00f957c700c175ac55756fd
luciana luize <lucianaluize2016@gmail.com>
2026-07-14 20:18:42 -0300

    adiciona registerPage e atualiza loginPage e userData.json

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -8,17 +8,21 @@ class LoginPage {
         loginButton:'[data-test="signin-submit"]',
         boxRemember:'[name="remember"]',
         TextSignUp:'[data-test="signup"]',
-        
-
+      
     }
          return selectors
   }
+
      accessLoginPage(){
-    
+      cy.visit('/signin')
 
+  }
+     loginWithUser(username, password){
+      cy.get(this.selectorsList().userNameField).type(username)
+      cy.get(this.selectorsList().passwordField).type(password)
+      cy.get(this.selectorsList().loginButton).click()
 
- }
-
+    }
 
 }
 
---
+++ cypress/pages/registrerPage.js
@@ -0,0 +1,16 @@
+class LoginPage {
+  selectorsList(){
+
+    const selectors = {
+
+      
+      
+    }
+         return selectors
+  }
+
+     accessLoginPage(){
+      cy.visit('/signup')
+     }
+    
+}
